4911676be68571ae938b0d02089e9e98e0391c53
zero8103 <mossaddak15-2413@diu.edu.bd>
2023-07-29 17:32:23 +0600

    added django silk

--- object_app/urls.py
+++ object_app/urls.py
@@ -1,11 +1,12 @@
-
 from django.contrib import admin
-from django.urls import path
-from .import views
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('addblog/', views.AddBlog, name="addblog"),
-    path('BlogApdateView/<int:pk>/', views.BlogApdateView, name="BlogApdateView"),
-    path('UpdateBlog/<int:pk>/', views.UpdateBlog, name="UpdateBlog"),
+    path("", views.home, name="home"),
+    path("addblog/", views.AddBlog, name="addblog"),
+    path("BlogApdateView/<int:pk>/", views.BlogApdateView, name="BlogApdateView"),
+    path("UpdateBlog/<int:pk>/", views.UpdateBlog, name="UpdateBlog"),
 ]
+
+urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
